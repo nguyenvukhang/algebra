beca8ee9e10bb99c2110bd8160fd6ba4dd1f4742
nguyenvukhang <brew4k@gmail.com>
2026-03-24 11:52:19 +0800

    ...

--- Algebra/DummitFoote/ec59f88.lean
+++ Algebra/DummitFoote/ec59f88.lean
@@ -2,13 +2,20 @@
 
 import Algebra.Prelude
 
-open Function
+open Function Finset
 
 universe u v
 
 variable {G : Type u} {H : Type v}
-  
+
 section Page43
+-- A quick warmup: if f is injective on a finite set A, then the image of A
+-- under f has the same cardinality as A. That is, |f(A)| = |A|. We can prove
+-- this intuitively with pen and paper using induction to build A up from an
+-- empty set.
+example [DecidableEq G] (A : Finset G) (f : G → G) (hf : Set.InjOn f A)
+  : #(A.image f) = #A := card_image_iff.mpr hf
+  
 -- page 43 "since these groups have the same order, this map must also be
 -- surjective".
 variable [h₁ : Fintype G] [h₂ : Fintype H] (φ : G → H)
